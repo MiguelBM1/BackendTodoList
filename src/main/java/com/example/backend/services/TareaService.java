544502d18deb7547c9f994682d17fead5b780dad
MiguelBM1 <bm15056@ues.edu.sv>
2024-04-14 15:33:53 -0600

    comentarios

--- src/main/java/com/example/backend/services/TareaService.java
+++ src/main/java/com/example/backend/services/TareaService.java
@@ -5,9 +5,7 @@ import com.example.backend.dto.requests.EditarTareaRequest;
 import com.example.backend.dto.response.ListarEstadoResponse;
 import com.example.backend.dto.response.ListarPersonasResponse;
 import com.example.backend.dto.response.ListarTareaResponse;
-import com.example.backend.entities.Personas;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 
@@ -15,6 +13,7 @@ public interface TareaService {
 
 
     List<ListarTareaResponse> obtenerTareas();
+
     ListarTareaResponse crearTareas(CrearTareaRequest request);
 
     ListarTareaResponse obtenerTarea(String id);
--- src/main/java/com/example/backend/web/TareaController.java
+++ src/main/java/com/example/backend/web/TareaController.java
@@ -14,40 +14,49 @@ public class TareaController {
 
     private final TareaService tareaService;
 
+    //obtener combo de personas
     @GetMapping("/personas")
     public ResponseEntity<?> getPersonas() {
         return ResponseEntity.ok(this.tareaService.obtenerPersonas());
     }
 
+    //obtener combo de estados
     @GetMapping("/estados")
     public ResponseEntity<?> getEstados() {
         return ResponseEntity.ok(this.tareaService.obtenerEstados());
     }
+
+    //obtener todas las tareas
     @GetMapping
     public ResponseEntity<?> obtenerTareas() {
         return ResponseEntity.ok(tareaService.obtenerTareas());
     }
 
+    //obtener una tarea por id
     @GetMapping("/{id}")
     public ResponseEntity<?> obtenerTarea(@PathVariable String id) {
         return ResponseEntity.ok(tareaService.obtenerTarea(id));
     }
 
+    //crear una tarea
     @PostMapping
     public ResponseEntity<?> crearTarea(@RequestBody CrearTareaRequest request) {
         return ResponseEntity.ok(tareaService.crearTareas(request));
     }
 
+    //actualizar una tarea
     @PutMapping("/{id}")
     public ResponseEntity<?> actualizarTarea(@PathVariable String id, @RequestBody EditarTareaRequest request) {
         return ResponseEntity.ok(tareaService.actualizarTareas(id, request));
     }
 
+    //actualizar el estado de una tarea
     @PutMapping("/estado/{id}")
     public ResponseEntity<?> actualizarEstadoTarea(@PathVariable String id) {
         return ResponseEntity.ok(tareaService.actualizarEstadoTareas(id));
     }
 
+    //eliminar una tarea
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarTarea(@PathVariable String id) {
         tareaService.eliminarTareas(id);
